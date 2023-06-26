af7a132a754803c51ef91adaa5883bac142d9db9
Olt Gashi <68893935+oltgvshi@users.noreply.github.com>
2023-06-26 04:49:33 +0200

    project setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-home>
+    <main class="container home">
+        <h1>Mindf@#$. <small>OPTICAL ILLUSIONS & MIND BENDING</small></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates tenetur quod quae porro dolores vero animi obcaecati, rerum nostrum deleniti iusto quo voluptatem et minus, incidunt harum aut autem.</p>
+    </main>
+</x-home>
